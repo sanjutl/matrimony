fb7c1f9fa0ac589e3a249542b8c02bf3e54ea78c
gokul <gokulsunil2015@gmail.com>
2025-03-17 13:36:03 +0530

    euro

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -127,6 +127,7 @@ function Dashboard() {
         `${baseUrl}:8000/api/v1/user/usercarddetails/${userId}`
       );
       setUserProfile(response.data.data);
+      
     } catch (error) {
       console.log("error", error);
     }
@@ -312,21 +313,21 @@ function Dashboard() {
     {
       title: "Silver",
       description: "3 month plan",
-      price: "₹4,500",
+      price: "£4,500",
       discount: "10% OFF",
       comingSoon: true,
     },
     {
       title: "Gold",
       description: "3 month plan",
-      price: "₹4,300",
+      price: "£4,300",
       discount: "12% OFF",
       comingSoon: true,
     },
     {
       title: "Platinum",
       description: "6 month plan",
-      price: "₹6,500",
+      price: "£6,500",
       discount: "",
       comingSoon: true,
     },
@@ -349,8 +350,8 @@ function Dashboard() {
   }, []);
 
   const myRef = useRef([]);
-  const observerRef = useRef(null); // ✅ Ensure it's null initially
-  const headingRef = useRef([]); // ✅ Separate ref for heading
+  const observerRef = useRef(null); 
+  const headingRef = useRef([]);
 
   useEffect(() => {
     if (!observerRef.current) {
