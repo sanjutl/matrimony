404b0625aaf32e8728b539d37ca2c863f8065431
gokul <gokulsunil2015@gmail.com>
2025-03-18 12:10:13 +0530

    uk

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://13.202.92.165:3000",
+    origin: "https://ukezhavamatrimony.com",
     methods: ["GET", "POST"],
     credentials: true,
   },
